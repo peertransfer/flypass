c2f94e1f0a3bf2a0d36588c6e640167d5b103734
Marco Alacot <marcoalacot@gmail.com>
2015-11-27 13:54:46 +0100

    Authorization service grant and revoke

--- app/domain/authorization_service.rb
+++ app/domain/authorization_service.rb
@@ -1,1 +1,17 @@
-class AuthorizationService; end
+class AuthorizationService
+  def self.update_for_user(user_id, credential_ids)
+    current = Authorization.where(user_id: user_id).map(&:credential_id)
+
+    to_revoke = current - credential_ids
+    to_grant = credential_ids - current
+
+    to_revoke.each do |credential_id|
+      authorization = Authorization.find_by(credential_id: credential_id, user_id: user_id)
+      authorization.destroy
+    end
+
+    to_grant.each do |credential_id|
+      Authorization.create(user_id: user_id, credential_id: credential_id)
+    end
+  end
+end
--- spec/domain/authorization_service_spec.rb
+++ spec/domain/authorization_service_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe AuthorizationService do
-  it 'foo' do
-    expect(true).to eq(true)
+  it 'grants a credential for a user' do
+    AuthorizationService.update_for_user(1, [1])
+
+    authorization = Authorization.where(user_id: 1, credential_id: 1)
+    expect(authorization).to be_present
+  end
+
+  it 'revokes a credential for a user' do
+    Authorization.create(user_id: 1, credential_id: 1)
+
+    AuthorizationService.update_for_user(1, [])
+    authorization = Authorization.where(user_id: 1, credential_id: 1)
+
+    expect(authorization).to be_empty
   end
 end
