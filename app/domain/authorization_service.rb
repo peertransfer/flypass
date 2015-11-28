398fe93c2e0ca6f5d2eb34a73efd431d070b52a0
Marco Alacot <marcoalacot@gmail.com>
2015-11-28 16:28:03 +0100

    WIP

--- app/domain/authorization_service.rb
+++ app/domain/authorization_service.rb
@@ -11,13 +11,19 @@ class AuthorizationService
 
   def self.grant(user_id, credential_ids)
     credential_ids.each do |credential_id|
-      Authorization.create(user_id: user_id, credential_id: credential_id)
+      Authorization.create(
+        user_id: user_id,
+        credential_id: credential_id
+      )
     end
   end
 
   def self.revoke(user_id, credential_ids)
     credential_ids.each do |credential_id|
-      authorization = Authorization.find_by(credential_id: credential_id, user_id: user_id)
+      authorization = Authorization.find_by(
+        credential_id: credential_id,
+        user_id: user_id
+      )
       authorization.destroy
 
       credential = Credential.find(credential_id)
@@ -26,7 +32,11 @@ class AuthorizationService
 
       new_password = SecureRandom.urlsafe_base64
 
-      plugin_class.change_password(credential.username, credential.password, new_password)
+      plugin_class.change_password(
+        credential.username,
+        credential.password, new_password
+      )
+
       credential.update_attributes(password: new_password)
     end
   end
